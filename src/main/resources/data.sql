d8205770bcd20fe92f9e28d3326b7930441f9067
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2017-07-30 23:06:53 +0200

    Changed start info in data base

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (user_id, username, password, first_name, last_name, date_of_birth) VALUES
-  ('1', 'Asterisk', '123456', 'Nata', 'Leonova', '1983-11-18'),
-  ('2', 'Sonic', 'haj55', 'Oleg', 'Sonov', '2000-01-22'),
-  ('3', 'Yupie', '222555', 'Yura', 'Privezentsev', '1985-03-22');
+  ('1', 'Sociopath', 'desire', 'Cersei', 'Lannister', '1000-10-10'),
+  ('2', 'The Kingslayer', 'one-handed', 'Jaime', 'Lannister', '1000-10-10'),
+  ('3', 'MotherOfDragon', 'justice', 'Daenerys', 'Targaryen', '1010-03-31');
 
 INSERT INTO groups (group_id, name_of_group) VALUES
-  ('1', 'Group One'),
-  ('2', 'Group Two');
+  ('1', 'Queen'),
+  ('2', 'Blood mixers');
 
 INSERT INTO groups_users (group_id, user_id) VALUES
 	('1', '1'),
